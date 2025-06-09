afede3294c4c73a25adc060f7148bd4ccfcb4633
eholuans <web3luan@gmail.com>
2025-06-09 13:21:17 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
-// Arquivo JavaScript principal
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Aplicação inicializada');
-    
-    // Funções adicionais podem ser adicionadas aqui conforme necessário
+
 });
